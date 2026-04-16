5b72d5d5b00b788d6b8d0145d5603e113d875adf
Blaner Krisztián <barkiza760bc@gmail.com>
2026-04-16 14:01:26 +0200

    Added TaskItem with basic errorhandling

---
+++ LifeTrackerApp/LifeTrackerApp/Models/TaskItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeTrackerApp.Models
+{
+    internal class TaskItem
+    {
+        private static int ID { get; set; }
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        public TaskItem(string title, string priority, DateTime duedate)
+        {
+            if (title.Trim() == "" || !(priority != "Low" || priority != "Medium" || priority != "High") || DateTime.Now > duedate)
+            {
+                throw new ArgumentException();
+            }
+
+        }
+    }
+}
