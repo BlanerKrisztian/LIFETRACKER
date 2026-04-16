68a7492bbbb936c313e59cc7412c0fbad05804d1
Wustinger.Csongor <Wustinger.Csongor@C19-07>
2026-04-16 14:30:04 +0200

    Created HabitRecords.cs

---
+++ LifeTrackerApp/LifeTrackerApp/HabitRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeTrackerApp
+{
+    public class HabitRecord
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Frequency { get; set; }
+        public List<DateTime> CompletionLog { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ LifeTrackerApp/LifeTrackerApp/HabitTracker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+//using LifeTrackerApp.Models;
+
+namespace LifeTrackerApp
+{
+    public class HabitTracker
+    {
+        List<HabitRecord> Habits = new List<HabitRecord>();
+        public HabitRecord AddHabit(string name, string frequency)
+        {
+            Habits.Append(new HabitRecord { Id = 0, Name = name, Frequency = frequency, CompletionLog = [], CreatedAt = DateTime.Now });
+        }
+    }
+}
