6922c3ce19d4a257c8bb24de693e8e2535e2f01c
Blaner Krisztián <barkiza760bc@gmail.com>
2026-04-16 14:25:37 +0200

    Dveloped Task Manager

--- LifeTrackerApp/LifeTrackerApp/Data/TaskManager.cs
+++ LifeTrackerApp/LifeTrackerApp/Data/TaskManager.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LifeTrackerApp.Models;
 
 namespace LifeTrackerApp.Data
 {
-    internal class TaskManager
+    public class TaskManager
     {
+        static private List<TaskItem> taskItems { get; set; }
+        static private int ID = 1;
+
+        static TaskItem AddTask(string title, string priority, DateTime duedate)
+        {
+            TaskItem newTask = new(ID, title, priority, duedate);
+            ID++;
+            taskItems.Add(newTask);
+            return newTask;
+        }
+        static bool RemoveTask(int id)
+        {
+            TaskItem TaskToDelete = taskItems.Where(Task => Task.Id == id).First();
+            if (TaskToDelete is null)
+                return false;
+            taskItems.Remove(TaskToDelete);
+            return true;
+        }
+
     }
 }
